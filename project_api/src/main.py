cf6ae37cdf75011216f7a0ccb8f4b0cfb6d3a3a1
Jakub Stec <stecu03@gmail.com>
2026-01-11 21:09:22 +0100

    .

--- project_api/src/main.py
+++ project_api/src/main.py
@@ -4,6 +4,8 @@ from src.config.logging import configure_logging
 from fastapi import FastAPI
 # IMPORT CORSMiddleware
 from fastapi.middleware.cors import CORSMiddleware 
+from slowapi.errors import RateLimitExceeded
+from slowapi import _rate_limit_exceeded_handler
 
 configure_logging("DEBUG")
 
@@ -21,12 +23,16 @@ origins = [
     "http://localhost:5173", # Vite (React) default port
     "http://localhost:3000", # Create React App default port
     "http://localhost:8000",
+    "http://127.0.0.1:5173",
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:8000",
     "https://frontend-production-6ee7.up.railway.app", # Deployed frontend URL
 ]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
+    allow_origin_regex="https://.*\.up\.railway\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -35,6 +41,7 @@ app.add_middleware(
 
 app.include_router(auth_controller, prefix=STANDARD_PREFIX)
 app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 app.include_router(files_controller, prefix=STANDARD_PREFIX)
 
 @app.get("/ping")
